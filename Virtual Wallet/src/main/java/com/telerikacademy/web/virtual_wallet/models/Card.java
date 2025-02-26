f0e71df9acb32ea343fea288867aadc86d8a1a16
Lachezar Lazarov <latschezar04@gmail.com>
2025-02-26 18:01:58 +0200

    Modify User and Card entities

--- Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/models/Card.java
+++ Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/models/Card.java
@@ -1,79 +1,49 @@
 package com.telerikacademy.web.virtual_wallet.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+import lombok.Data;
 
+import java.time.LocalDate;
+
+@Data
 @Entity
+@Table(name = "cards")
 public class Card {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
-    @NotEmpty(message = "Card number can't be empty!")
-    @Pattern(
-            regexp = ".\\d{16}",
-            message = "Card number must be 16 digits!"
-    )
-    @Column(unique = true, length = 16)
+    @NotNull(message = "Card number is required.")
+    @Pattern(regexp = "^\\d{16}$", message = "Card number must be exactly 16 digits.")
+    @Column(name = "card_number")
     private String cardNumber;
 
-    @NotEmpty(message = "Expiration date can't be empty!")
-    @Future(message = "Expiration date must be in the future.")
-    private String expiryDate;
+    @NotNull(message = "Expiration date is required.")
+    @Column(name = "expiration_Date")
+    private LocalDate expirationDate;
 
-    @NotEmpty(message = "Card holder name can't be empty!")
-    @Size(min = 2, max = 30, message = "Card holder name must be between 2 and 30 symbols!")
-    private String cardHolderName;
+    @NotNull(message = "Check number is required.")
+    @Pattern(regexp = "^\\d{3}$", message = "Check number must be exactly 3 digits.")
+    @Column(name = "check_Number")
+    private String checkNumber;
 
-    @NotEmpty(message = "CVV can't be empty!")
-    @Pattern(
-            regexp = ".\\d{3}",
-            message = "CVV must be 3 digits!"
-    )
-    @Column(length = 3)
-    private String cvv;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    public Long getId() {
-        return id;
+    @PrePersist
+    protected void onCreate() {
+        if (expirationDate == null) {
+            expirationDate = LocalDate.now().plusYears(5);
+        }
     }
 
-    public void setId(long id) {
-        this.id = id;
+    @Transient
+    public String getCardHolder() {
+        return user.getFullName();
     }
 
-    public String getCvv() {
-        return cvv;
-    }
-
-    public void setCvv(String cvv) {
-        this.cvv = cvv;
-    }
-
-    public String getCardHolderName() {
-        return cardHolderName;
-    }
-
-    public void setCardHolderName(String cardHolderName) {
-        this.cardHolderName = cardHolderName;
-    }
-
-    public String getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(String expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
 }
--- Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/models/User.java
+++ Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/models/User.java
@@ -1,26 +1,34 @@
 package com.telerikacademy.web.virtual_wallet.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import java.util.Set;
 
+@Data
 @Entity
-@Table(name = "users", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "username"),
-        @UniqueConstraint(columnNames = "email"),
-        @UniqueConstraint(columnNames = "phoneNumber")
-})
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @Column(name = "id")
+    private long id;
 
     @NotEmpty(message = "Username can't be empty.")
     @Size(min = 2, max = 20, message = "Username must be between 2 and 20 symbols.")
+    @Column(name = "username")
     private String username;
+
+    @NotEmpty(message = "First name can't be empty.")
+    @Size(min = 1, max = 16, message = "First name must be between 1 and 16 characters.")
+    @Column(name = "first_Name")
+    private String firstName;
+
+    @NotEmpty(message = "Last name can't be empty.")
+    @Size(min = 1, max = 16, message = "Last name must be between 1 and 16 characters.")
+    @Column(name = "last_Name")
+    private String lastName;
 
     @NotEmpty(message = "Password can't be empty.")
     @Size(min = 8, message = "Password must be at least 8 symbols long.")
@@ -28,11 +36,12 @@ public class User {
             regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).*$",
             message = "Password must contain at least one uppercase letter, one digit, and one special character."
     )
+    @Column(name = "password")
     private String password;
 
     @NotEmpty(message = "Email can't be empty.")
     @Email(message = "Email must be valid.")
-    @Column(nullable = false, unique = true)
+    @Column(name = "email")
     private String email;
 
     @NotEmpty(message = "Phone number can't be empty.")
@@ -40,7 +49,7 @@ public class User {
             regexp = "(?=.*\\d{10})",
             message = "Phone number must be 10 digits!"
     )
-    @Column(nullable = false, unique = true)
+    @Column(name = "phone")
     private String phone;
 
     @NotEmpty(message = "Card can't be empty.")
@@ -48,77 +57,30 @@ public class User {
             regexp = ".\\d{16}",
             message = "Card number must be 16 digits!"
     )
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, unique = true)
-    private Card card;
 
     @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "photo")
     private byte[] photo;
 
-    private boolean isAdmin;
+    @Column(name = "isAdmin")
+    private Boolean isAdmin;
 
+    @Column(name = "isBlocked")
+    private Boolean isBlocked;
 
-    public int getId() {
-        return id;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Set<Card> cards;
+
+    @AssertTrue(message = "Full name must be between 2 and 32 characters.")
+    public boolean isFullNameValid() {
+        int length = (firstName + " " + lastName).trim().length();
+        return length >= 2 && length <= 32;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    @Transient
+    public String getFullName() {
+        return firstName + " " + lastName;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Card getCard() {
-        return card;
-    }
-
-    public void setCard(Card card) {
-        this.card = card;
-    }
-
-    public byte[] getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(byte[] photo) {
-        this.photo = photo;
-    }
-
-    public boolean isAdmin() {
-        return isAdmin;
-    }
-
-    public void setAdmin(boolean admin) {
-        isAdmin = admin;
-    }
 }
