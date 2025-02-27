9554d2367ac2dcae404e8c2158809ece4506617b
Lachezar Lazarov <latschezar04@gmail.com>
2025-02-27 12:40:26 +0200

    Update .gitignore

--- Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/config/HibernateConfig.java
+++ Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/config/HibernateConfig.java
@@ -28,7 +28,7 @@ public class HibernateConfig {
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("com.telerikacademy.web.forumsystem.models");
+        sessionFactory.setPackagesToScan("com.telerikacademy.web.virtual_wallet.models");
         sessionFactory.setHibernateProperties(hibernateProperties());
         return sessionFactory;
     }
