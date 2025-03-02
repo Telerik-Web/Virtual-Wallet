c17ce844f0e9b47c37a14049a32054966db56186
ivan200126y <ivan29654@gmail.com>
2025-03-02 15:16:10 +0200

    Proper error display in the register form

--- Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/controllers/mvc/AuthenticationMvcController.java
+++ Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/controllers/mvc/AuthenticationMvcController.java
@@ -16,10 +16,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/auth")
@@ -79,7 +83,7 @@ public class AuthenticationMvcController {
 
     @PostMapping("/register")
     public String processRegister(@Valid @ModelAttribute("register") UserDTO registerDto,
-                                  BindingResult errors) {
+                                  BindingResult errors, Model model) {
         if (errors.hasErrors()) {
             return "Register";
         }
