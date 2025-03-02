ec3b84d9800844cd78b33fbfbbc8196a017a3d2a
ivan200126y <ivan29654@gmail.com>
2025-03-02 23:55:34 +0200

    Test commit

--- Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/VirtualWalletApplication.java
+++ Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/VirtualWalletApplication.java
@@ -3,12 +3,10 @@ package com.telerikacademy.web.virtual_wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.telerikacademy.web.virtual_wallet.models")
 public class VirtualWalletApplication {
-//jjjj
     public static void main(String[] args) {
         SpringApplication.run(VirtualWalletApplication.class, args);
     }
