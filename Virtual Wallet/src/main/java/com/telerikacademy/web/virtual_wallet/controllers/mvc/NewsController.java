955c59c11f6b9d1d78431d343bee703c88881dd1
ivan200126y <ivan29654@gmail.com>
2025-03-04 11:02:33 +0200

    Implementing Financial News api for the logged in homeView

--- Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/controllers/mvc/NewsController.java
+++ Virtual Wallet/src/main/java/com/telerikacademy/web/virtual_wallet/controllers/mvc/NewsController.java
@@ -1,21 +1,25 @@
-//package com.telerikacademy.web.virtual_wallet.controllers.mvc;
-//
-//import com.telerikacademy.web.virtual_wallet.FinancialNewsFetcher;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping("/api/news")
-//public class NewsController {
-//
-//    @GetMapping
-//    public List<Map<String, String>> getNews() {
-//        // Fetch news articles
-//        List<Map<String, String>> articles = FinancialNewsFetcher.fetchFinancialNews();
-//        return articles;
-//    }
-//}
+package com.telerikacademy.web.virtual_wallet.controllers.mvc;
+
+import com.telerikacademy.web.virtual_wallet.FinancialNewsFetcher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/news")
+public class NewsController {
+
+    private final FinancialNewsFetcher financialNewsFetcher;
+
+    public NewsController(FinancialNewsFetcher financialNewsFetcher) {
+        this.financialNewsFetcher = financialNewsFetcher;
+    }
+
+    @GetMapping
+    public List<String> getLatestNews() {
+        return financialNewsFetcher.fetchLatestNews();
+    }
+}
